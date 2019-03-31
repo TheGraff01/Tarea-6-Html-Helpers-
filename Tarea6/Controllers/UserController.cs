d8f26244337d671f0609f2141c0a960143a60cec
Edgar Montilla <thegraffit01@gmail.com>
2019-03-30 22:08:09 -0400

    La Tarea 6

---
+++ Tarea6/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Tarea6.Models;
+
+namespace Tarea6.Controllers
+{
+    public class UserController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View(new User());
+        }
+
+        public ActionResult Formulario()
+        {
+            return View(new User());
+        }
+
+        public ActionResult Perfil(User user)
+        {
+            return View(user);
+        }
+
+        [HttpPost]
+        public ActionResult Formulario(User user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(user);
+            }
+            return View("Perfil", user);
+        }
+    }
+}
---
+++ Tarea6/Models/User.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Tarea6.Models
+{
+    public class User
+    {
+        [DisplayName("Cédula:")]
+        [Required]
+        public int Cedula { get; set; }
+
+        [DisplayName("Nombre:")]
+        [Required]
+        public string Nombre { get; set; }
+
+        [DisplayName("Apellido:")]
+        [Required]
+        public string Apellido { get; set; }
+
+        [DisplayName("Edad:")]
+        [Range(15, 120)]
+        public int Edad { get; set; }
+
+        [DisplayName("Teléfono:")]
+        public int Telefono { get; set; }
+
+        [DisplayName("Correo Electrónico:")]
+        [EmailAddress]
+        public string Correo { get; set; }
+
+        [DisplayName("Género:")]
+        public string Genero { get; set; }
+
+        [DisplayName("Estado Civil:")]
+        public string EstadoCivil { get; set; }
+
+        [DisplayName("Hobbys:")]
+        public string[] Hobbys { get; set; }
+
+
+
+    }
+}
